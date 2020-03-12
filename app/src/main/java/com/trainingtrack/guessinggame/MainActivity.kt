95cbb6eca0f64960b5f88e75c0877f17ed3d0baa
ghigbie <gehigbie@gmail.com>
2020-03-11 19:38:22 -0700

    Add new activity

--- app/src/main/java/com/trainingtrack/guessinggame/MainActivity.kt
+++ app/src/main/java/com/trainingtrack/guessinggame/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.trainingtrack.guessinggame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        guessButton.setOnClickListener {
+            intent = Intent(this, )
+        }
     }
+
 }
---
+++ app/src/main/java/com/trainingtrack/guessinggame/ShowGuess.kt
@@ -0,0 +1,12 @@
+package com.trainingtrack.guessinggame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowGuess : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_guess)
+    }
+}
