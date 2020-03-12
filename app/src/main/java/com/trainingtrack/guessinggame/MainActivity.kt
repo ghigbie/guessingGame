0a5e81339b1a260605ec929de278b989a668a265
ghigbie <gehigbie@gmail.com>
2020-03-11 20:52:20 -0700

    Add toast

--- app/src/main/java/com/trainingtrack/guessinggame/MainActivity.kt
+++ app/src/main/java/com/trainingtrack/guessinggame/MainActivity.kt
@@ -3,6 +3,7 @@ package com.trainingtrack.guessinggame
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,10 +15,12 @@ class MainActivity : AppCompatActivity() {
 
         guessButton.setOnClickListener {
             val guess = editText.text.toString().trim()
-            if(guess != null) {
+            if(!guess.isEmpty()) {
                 intent = Intent(this, ShowGuess::class.java)
                 intent.putExtra("guess", guess)
                 startActivity(intent)
+            }else{
+                Toast.makeText(this, "Enter guess", Toast.LENGTH_SHORT)
             }
         }
     }
