321f8b30b345f08918999cc7a30d6f3da5c85b53
ghigbie <gehigbie@gmail.com>
2020-03-15 17:44:14 -0700

    Final push

--- app/src/main/java/com/trainingtrack/guessinggame/MainActivity.kt
+++ app/src/main/java/com/trainingtrack/guessinggame/MainActivity.kt
@@ -9,7 +9,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    val REQUEST_CODE: Int = 2
+    private val REQUEST_CODE: Int = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,16 +26,16 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Enter guess", Toast.LENGTH_SHORT)
             }
         }
+
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if(requestCode == REQUEST_CODE){
-            if(requestCode == Activity.RESULT_OK){
-                val message: String? = data?.getStringExtra("message")
-                Toast.makeText(this, message, Toast.LENGTH_SHORT )
-            }
+            val message: String? = data?.getStringExtra("message")
+            Toast.makeText(this, message, Toast.LENGTH_SHORT )
         }
     }
 
